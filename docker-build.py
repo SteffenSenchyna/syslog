7ec73aa6e8f133dd710490f13748d0906b7ad8e2
SteffenSenchyna <105737839+SteffenSenchyna@users.noreply.github.com>
2023-03-27 01:14:17 -0600

    Updated Dockerfile and compose

--- docker-build.py
+++ docker-build.py
@@ -2,10 +2,12 @@ import os
 
 # Define the image name and tag
 image_name = 'ssenchyna/syslog'
-image_tag = 'latest'
-# Build the Docker image
+image_tag = '1.0'
+# Build the Docker image\
+os.system(f'docker compose down')
 os.system(f'docker build -t {image_name}:{image_tag} .')
-# Log in to Docker Hub
+# # Log in to Docker Hub
 os.system('docker login')
-# Push the Docker image to Docker Hub
+# # Push the Docker image to Docker Hub
 os.system(f'docker push {image_name}:{image_tag}')
+os.system(f'docker-compose up')
